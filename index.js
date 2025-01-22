c99192f8cdb91f73ae372c5205dfe3a02442eaf1
Isaac Scarpelli <140021795+IsaacScarpelli@users.noreply.github.com>
2025-01-22 22:51:50 +0000

    commit 1

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const productRoutes = require('./routes/productRoutes');
+
+const app = express();
+
+//Midlewaer
+app.use(express.json());
+
+//Conectando ao MongoDB
+mongoose
+  .connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() =>  console.log('Conectado ao MongoDB'))
+  .catch(err => console.log(err));
+
+//Rotas
+app.use('api/products', productRoutes);
+
+//Inicializador servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>(console.log(`Servidor rodando na porta ${PORT}`)))
