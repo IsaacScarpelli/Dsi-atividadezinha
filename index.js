f7ad5c20a13a0a3f44759ab3c6a07395ac83c464
Isaac Scarpelli <140021795+IsaacScarpelli@users.noreply.github.com>
2025-01-23 00:31:56 +0000

    commit 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const productRoutes = require('./routes/productRoutes');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
@@ -10,13 +10,13 @@ app.use(express.json());
 
 //Conectando ao MongoDB
 mongoose
-  .connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() =>  console.log('Conectado ao MongoDB'))
-  .catch(err => console.log(err));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Conectado ao MongoDB"))
+  .catch((err) => console.log(err));
 
 //Rotas
-app.use('api/products', productRoutes);
+app.use("api/products", productRoutes);
 
 //Inicializador servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>(console.log(`Servidor rodando na porta ${PORT}`)))
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,41 +1,44 @@
-const exprees = require('express');
-const router = exprees.Router();
-const Product = require('../models/Products');
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/Products");
 
 //Rota para cadastrar um novo produto
-router.post('/',async (req,res)=>{
-  try{
-    const{nome, tipo, quantidade} = req.body,
-      if(quantidade < 0){
-      return res.status(400).json({error: 'Quantidade inválida'})
-      }
-    const product = new Product({nome, tipo, quantidade});
+router.post("/", async (req, res) => {
+  try {
+    const { nome, tipo, quantidade } = req.body;
+    if (quantidade < 0) {
+      return res.status(400).json({ error: "Quantidade inválida" });
+    }
+    const product = new Product({ nome, tipo, quantidade });
     await product.save();
     res.status(201).json(product);
-  }catch(error){
-    res.status(500).json({error: 'Erro ao cadastrar produto'});
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao cadastrar produto" });
   }
 });
 
 //Rota para listar todos os produtos
-router.get('/', async (req,res)=>{
-  try{
+router.get("/", async (req, res) => {
+  try {
     const products = await Product.find();
     res.status(200).json(products);
-  }catch(error){
-    res.status(500).json({error: 'Erro ao listar produtos'});
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao listar produtos" });
     console.log(error);
   }
 });
 
 ///Rota para consultar o estoque total
-router.get('/total', async (req,res)=>{
-  try{
+router.get("/total", async (req, res) => {
+  try {
     const products = await Product.find();
-    const total = products.reduce((sum, product)=> sum + product.quantidade, 0)
-    res.status(200).json({total});
-  }catch(error){
-    res.status(500).json({error: 'Erro ao consultar estoque total'});
+    const total = products.reduce(
+      (sum, product) => sum + product.quantidade,
+      0,
+    );
+    res.status(200).json({ total });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao consultar estoque total" });
     console.log(error);
   }
 });
