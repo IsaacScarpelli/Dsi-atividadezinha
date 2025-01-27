53f2ddbd176e1913ed205299be9ededec1dc2ace
Isaac Scarpelli <140021795+IsaacScarpelli@users.noreply.github.com>
2025-01-27 19:33:14 +0000

    fodase

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const productRoutes = require("./routes/productRoutes");
-const site = require('./site.js');
+  require("dotenv").config();
+  const express = require("express");
+  const mongoose = require("mongoose");
+  const productRoutes = require("./routes/productRoutes");
+  const site = require('./site.js');
 
-const app = express();
+  const app = express();
 
-// Middleware para parsear JSON no corpo das requisições
-app.use(express.json());
+  // Middleware
+  app.use(express.json());
 
-// Conectando ao MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Conectado ao MongoDB"))
-  .catch((err) => console.log("Erro ao conectar ao MongoDB:", err));
+  // Conectando ao MongoDB
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Conectado ao MongoDB"))
+    .catch((err) => console.log("Erro ao conectar ao MongoDB:", err));
 
-// Rota principal
-app.get('/', (req, res) => {
-  res.json({
-    text: "testando!"
+  // Rotas
+
+  // Rota para a página do Admin
+  app.get('/admin/:id', (req, res) => {
+      const id = req.params.id;
+      res.send(site(id)); // Chamando a função 'site' e passando o 'id'
   });
-});
 
-// Rota para administrar o site (admin)
-app.get('/admin/*', (req, res) => {
-  res.send(site()); // Invocar a função site() para a resposta
-});
+  // Rota de API de produtos
+  app.use("/api/products", productRoutes);
 
-// Rota da API de produtos
-app.use("/api/products", productRoutes);
+  // Rota principal
+  app.get('/', (req, res) => {
+    res.json({
+      text: "testando!"
+    });
+  });
 
-// Inicializar o servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+  // Inicializar o servidor
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+ 
